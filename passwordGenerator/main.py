38ee85d316c206a7792930bd08fd82d93583d0f3
kkozel3492 <97856416+kkozel3492@users.noreply.github.com>
2022-10-06 11:27:31 -0500

    Add files via upload

---
+++ passwordGenerator/main.py
@@ -0,0 +1,13 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+#direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+def encrypt(wordText, numShift):
+    for i in wordText:
+        position = alphabet.index(letter)
+
+
+encrypt(wordText = text, numShift=shift)
